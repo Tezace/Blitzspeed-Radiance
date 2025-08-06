6f636c5b54916d7058d9834cf24a6f329d76c4d3
Tezace <118602543+Tezace@users.noreply.github.com>
2025-08-06 23:20:36 +0300

    Update roblox.py

--- roblox.py
+++ roblox.py
@@ -1,11 +1,22 @@
 import requests
 import time
-from colorama import Fore, Style, init
+from colorama import Fore, Back, Style, init
+import random
+
+available = 0
 
 init()
 
+print(Fore.YELLOW + "██╗░░██╗███████╗░█████╗░██╗░░░██╗███████╗███╗░░██╗██╗░░░░░██╗░░░██╗  ███╗░░██╗░█████╗░███╗░░░███╗███████╗  ░██████╗███╗░░██╗██╗██████╗░███████╗██████╗░" + Style.RESET_ALL)
+print(Fore.WHITE + "██║░░██║██╔════╝██╔══██╗██║░░░██║██╔════╝████╗░██║██║░░░░░╚██╗░██╔╝  ████╗░██║██╔══██╗████╗░████║██╔════╝  ██╔════╝████╗░██║██║██╔══██╗██╔════╝██╔══██╗" + Style.RESET_ALL)
+print(Fore.YELLOW + "███████║█████╗░░███████║╚██╗░██╔╝█████╗░░██╔██╗██║██║░░░░░░╚████╔╝░  ██╔██╗██║███████║██╔████╔██║█████╗░░  ╚█████╗░██╔██╗██║██║██████╔╝█████╗░░██████╔╝" + Style.RESET_ALL)
+print(Fore.WHITE + "██╔══██║██╔══╝░░██╔══██║░╚████╔╝░██╔══╝░░██║╚████║██║░░░░░░░╚██╔╝░░  ██║╚████║██╔══██║██║╚██╔╝██║██╔══╝░░  ░╚═══██╗██║╚████║██║██╔═══╝░██╔══╝░░██╔══██╗" + Style.RESET_ALL)
+print(Fore.YELLOW + "██║░░██║███████╗██║░░██║░░╚██╔╝░░███████╗██║░╚███║███████╗░░░██║░░░  ██║░╚███║██║░░██║██║░╚═╝░██║███████╗  ██████╔╝██║░╚███║██║██║░░░░░███████╗██║░░██║" + Style.RESET_ALL)
+print(Fore.WHITE + "╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚══╝╚══════╝░░░╚═╝░░░  ╚═╝░░╚══╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝  ╚═════╝░╚═╝░░╚══╝╚═╝╚═╝░░░░░╚══════╝╚═╝░░╚═╝" + Style.RESET_ALL)
+print(Fore.YELLOW + Back.WHITE + "Original made by clemouche and modified by tezace :)" + Style.RESET_ALL)
+
 def check_username(username):
-    url = f"https://auth.roblox.com/v1/usernames/validate?Username={username}&Birthday=2000-01-01"
+    url = f"https://auth.roblox.com/v1/usernames/validate?Username={username}&Birthday={random.randint(1980, 2000)}-{random.randint(1, 10)}-{random.randint(1, 10)}"
     try:
         response = requests.get(url)
         response_data = response.json()
@@ -15,6 +26,7 @@ def check_username(username):
             print(Fore.GREEN + f"VALID: {username}" + Style.RESET_ALL)
             with open("valid.txt", "a") as valid_file:
                 valid_file.write(username + "\n")
+            available += 1
         elif code == 1:
             print(Fore.LIGHTBLACK_EX + f"TAKEN: {username}" + Style.RESET_ALL)
         elif code == 2:
@@ -31,7 +43,28 @@ def main():
 
     for username in usernames:
         check_username(username)
-        time.sleep(0.05)
+        time.sleep(0.01)
+    
+    if available >= 3000:
+        print(Fore.WHITE + Back.LIGHTYELLOW_EX + f"Valid username amount: {available} (HEAVENLY!!!)" + Style.RESET_ALL)
+    elif available >= 1000:
+        print(Fore.CYAN + f"Valid username amount: {available} (MYTHICAL!!)" + Style.RESET_ALL)
+    elif available >= 500:
+        print(Fore.YELLOW + f"Valid username amount: {available} (Legendary!)" + Style.RESET_ALL)
+    elif available >= 250:
+        print(Fore.MAGENTA + f"Valid username amount: {available} (Epic!)" + Style.RESET_ALL)
+    elif available >= 100:
+        print(Fore.BLUE + f"Valid username amount: {available} (Rare!)" + Style.RESET_ALL)
+    elif available >= 25:
+        print(Fore.GREEN + f"Valid username amount: {available} (Uncommon)" + Style.RESET_ALL)
+    elif available >= 5:
+        print(Fore.BLACK + f"Valid username amount: {available} (Common)" + Style.RESET_ALL)
+    else:
+        print("No usernames available..")
+    
+    time.sleep(1000)
+
+    
 
 if __name__ == "__main__":
     main()
